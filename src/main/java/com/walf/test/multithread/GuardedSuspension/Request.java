c28e7d42aa6cb7b617b58d0568e0e64df68d0a0b
wupengfei <wupengfei1981@126.com>
2019-08-10 22:25:28 +0800

    阻塞模式 可以在一端进行异步处理，给另一端看起来是同步的。

---
+++ src/main/java/com/walf/test/multithread/GuardedSuspension/Request.java
@@ -0,0 +1,18 @@
+package com.walf.test.multithread.GuardedSuspension;
+
+
+public class Request {
+    private final String name;
+
+    public Request(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String toString() {
+        return "[ Request " + name + " ]";
+    }
+}
